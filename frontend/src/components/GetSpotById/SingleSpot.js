7524024519d662168ae26294cf2e2e52ffe151cf
DLiaw <DL0429922@gmail.com>
2022-11-14 11:14:26 -0500

    started get spot by id

--- frontend/src/components/GetSpotById/SingleSpot.js
+++ frontend/src/components/GetSpotById/SingleSpot.js
@@ -1,26 +1,42 @@
-
+import './SingleSpot.css'
 
 const singleSpot = ({ oneSpot }) => {
+    const images = oneSpot.SpotImages
+
+    if (!images) return null;
+
     return (
         <div className="singleSpotMainDiv">
-            <div className="singleSpotTop3divs">
-                <div>
-                    <div id="singleSpotdivText"></div>
-                    <div id="singleSpotdivReview"></div>
+            <div className="singleSpotTop4divs">
+                <div className="singleSpotTextdiv">
+                    <div id="singleSpotdivText"><h3>{oneSpot.name}</h3></div>
+                    <div id="singleSpotdivReview"><p>{oneSpot.numReviews}{oneSpot.avgStarRating}</p></div>
                 </div>
-                <div>
-                    <div>
-                        <img id="singleSpotMainImage" alt={oneSpot.name} src={oneSpot.SpotImages[0].url}></img>
+                <div className="singleSpotImagediv">
+                    <div className="singleSpotphotodiv">
+                        <img id='largeimg' alt={oneSpot.name} src={oneSpot.SpotImages[0].url}></img>
                     </div>
-                    <div>
-                        {/* <img alt={oneSpot.name} src={oneSpot.SpotImages[1].url}></img>
-                        <img alt={oneSpot.name} src={oneSpot.SpotImages[2].url}></img>
-                        <img alt={oneSpot.name} src={oneSpot.SpotImages[3].url}></img>
-                        <img alt={oneSpot.name} src={oneSpot.SpotImages[4].url}></img> */}
+                    <div className="singleSpotSmalldiv">
+                        <div id='smallpic2'>
+                            <div>
+                                <img id='smallimg' alt={oneSpot.name} src={oneSpot.SpotImages[1].url}></img>
+                            </div>
+                            <div>
+                                <img id='smallimg' alt={oneSpot.name} src={oneSpot.SpotImages[2].url}></img>
+                            </div>
+                            {/* </div> */}
+                            {/* <div id='smallpic2'> */}
+                            {/* <div> */}
+                            <img id='smallimg' alt={oneSpot.name} src={oneSpot.SpotImages[3].url}></img>
+                            {/* </div> */}
+                            <div>
+                                <img id='smallimg' alt={oneSpot.name} src={oneSpot.SpotImages[4].url}></img>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
